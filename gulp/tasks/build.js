c7533e57d6254bac13572c292efb055260a3b5f3
Narongchai Thongkongkeaw <knodejs@gmail.com>
2016-07-20 09:02:09 +0700

    config gulp serveprd

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,3 +1,11 @@
 var gulp = require('gulp');
 
 gulp.task('build', ['browserify', 'markup']);
+
+gulp.task('serveprod', function() {
+  connect.server({
+    root: build,
+    port: process.env.PORT || 5000,
+    livereload: false
+  });
+});
