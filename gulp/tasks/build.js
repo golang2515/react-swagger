41f32c20b03098804abe46bec3efc7936a85b8f6
Narongchai Thongkongkeaw <golang.2515@gmail.com>
2016-07-20 22:27:15 +0700

    remove gulp serve

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,11 +1,2 @@
 var gulp = require('gulp');
-var connect = require('gulp-connect');
 gulp.task('build', ['browserify', 'markup']);
-
-gulp.task('serveprod', function() {
-  connect.server({
-    root: 'build',
-    port: process.env.PORT || 8000,
-    livereload: false
-  });
-});
