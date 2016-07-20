57b676bb3a4a8121ed34a1f1459872b525162a4a
Narongchai Thongkongkeaw <knodejs@gmail.com>
2016-07-20 09:21:55 +0700

    prepare deploy heroku

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
-
+var connect = require('gulp-connect');
 gulp.task('build', ['browserify', 'markup']);
 
 gulp.task('serveprod', function() {
   connect.server({
-    root: build,
-    port: process.env.PORT || 5000,
+    root: 'build',
+    port: process.env.PORT || 8000,
     livereload: false
   });
 });
