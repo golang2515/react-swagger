5846a271d52ba66fbbbe316cdd5eb72bf07236e9
Narongchai Thongkongkeaw <golang.2515@gmail.com>
2016-07-31 09:39:03 +0700

    update git config

--- src/app/config/constant.js
+++ src/app/config/constant.js
@@ -12,7 +12,7 @@ export const Config = {
     MODULE_NAME: 'APIs',
     SWAGGER_JSON_API: 'api/swagger-navigo.json',
     POWERED_BY: 'golang2515@gmail.com',
-    GIT_URL: '',
+    GIT_URL: 'https://github.com/golang2515/react-swagger.git',
     LAYOUT: {
         PADDING_LEFT: 250,
         SHOW_TOP: true,
